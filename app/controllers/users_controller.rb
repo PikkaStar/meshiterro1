15dfdae3be9690a82ed39f6308b3ad1f9cf05d4c
PikkaStar <yokotaka1226@gmail.com>
2023-10-03 07:03:55 +0000

    finish 4-22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,17 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
   
   def update
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+    
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
